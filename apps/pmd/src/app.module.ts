12867467f0cc87a53904174ad978a26e67e47382
Nikola Kescec <nkescec@croz.net>
2022-12-17 18:24:11 +0100

    Creating dockerfile.

--- apps/pmd/src/app.module.ts
+++ apps/pmd/src/app.module.ts
@@ -3,9 +3,10 @@ import { ToolController } from './controller/tool.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PmdToolService } from './service/pmd.tool.service';
 import ExecutorService from 'wrappers/common/service/executor.service';
+import * as process from 'process';
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: './apps/pmd/.env' })],
+  imports: [ConfigModule.forRoot({ envFilePath: process.env.NODE_ENV === 'production' ? './.env' : './apps/pmd/.env' })],
   controllers: [ToolController],
   providers: [PmdToolService, ExecutorService],
 })
